5ba13e5c18d0c39ebe2e09d4aedcc3a65a73c010
TheNeerajaAnand <neeraja.anand.here@gmail.com>
2021-03-04 18:50:12 +0530

    Stage 1 completed

--- lights-out/src/Board.jsx
+++ lights-out/src/Board.jsx
@@ -2,10 +2,13 @@ import React, {useState} from "react";
 import Cell from "./Cell";
 import './Board.css';
 
+
+
 function Board(props) {
   const [board, setBoard] = useState(createBoard(props));
   const [triesLeft, setTriesLeft] = useState(20);
   const [hasWon, setHasWon] = useState(false);
+  
 
   function createBoard() {
     let board = [];
@@ -15,31 +18,37 @@ function Board(props) {
         cellRow[column] = (Math.random() < props.chanceLightStartsOn); 
       }
       board[row] = cellRow; 
-    } 
+    }
+    
     return board
   }
+
+ 
+
   const flipCellsAround = (coord) => {
     let {nrows, ncols} = props;
     let [y, x] = coord.split("-").map(Number);
-  
+
 
     function flipCell(y, x) {
-      
+
+
       if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
         board[y][x] = !board[y][x];
       }
     }
+
     flipCell(y, x);
     flipCell(y - 1, x);
     flipCell(y + 1, x);
     flipCell(y, x - 1);
     flipCell(y, x + 1);
-    
+
     setBoard(board);
     setTriesLeft(triesLeft - 1);
     setHasWon(hasWon);
   }  
-  
+
   let table = new Array(board.length);
 
   for (let row = 0; row < table.length; row++) {
@@ -85,5 +94,5 @@ function Board(props) {
     );
 }
 
+
 export default Board;
-  
